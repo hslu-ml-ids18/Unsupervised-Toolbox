aad49aef397269f7c7392b6d872f0502c5667659
akandel <axel.kandel@gmail.com>
2019-10-11 10:32:14 +0200

    k-means clustering

--- app.R
+++ app.R
@@ -34,39 +34,69 @@ ui <- fluidPage(
                          "Number of Iterations:",
                          min = 2,
                          max = 20,
-                         value = 5)
+                         value = 5),
+            headerPanel('k-means clustering'),
+            sidebarPanel(
+                selectInput('xcol', 'X Variable', names(data)),
+                selectInput('ycol', 'Y Variable', names(data),
+                            selected=names(data)[[2]]),
+                numericInput('clusters', 'Cluster count', 3,
+                             min = 1, max = 9)
+            )
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
            plotOutput("distPlot"),
            #placeholder for data plot
-           plotOutput("data_plot")
+           plotOutput("tsne_plot"),
+           #  k-means clustering plot
+           plotOutput('plot1')
         )
     )
 )
 
-# Define a function to run tsna with input variables
-###t-Distributed Stochastic Neighbor Embedding (tSNE)
-library(Rtsne)
-# Use table row names to label the datapoint later in the plot:
-data_label<-as.factor(rownames(data))
-
-#remove duplicates:
-data_unique <- unique(data)
-
-# Run tSNE:
-# tSNEdata <- as.matrix(scale(data_unique))
-# tsne <- Rtsne(tSNEdata, dims = 2,
-#               perplexity= sliderInput$Perplexity, verbose=TRUE,
-#               max_iter = sliderInput$Iteration)
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-    # Create a PCA plot of the dataset 
-    output$data_plot <- tsneplot
-    
 
+    # Combine the selected variables into a new data frame
+    selectedData <- reactive({
+        data[, c(input$xcol, input$ycol)]
+    })
+    
+    clusters <- reactive({
+        kmeans(selectedData(), input$clusters)
+    })
+    
+    output$plot1 <- renderPlot({
+        palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
+                  "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
+        
+        par(mar = c(5.1, 4.1, 0, 1))
+        plot(selectedData(),
+             col = clusters()$cluster,
+             pch = 20, cex = 3)
+        points(clusters()$centers, pch = 4, cex = 4, lwd = 4)
+    })    
+    
+    # # Create a PCA plot of the dataset 
+    # output$tsne_plot <- renderPlot({
+    # # Define a function to run tsna with input variables
+    # ###t-Distributed Stochastic Neighbor Embedding (tSNE)
+    # library(Rtsne)
+    # # Use table row names to label the datapoint later in the plot:
+    # data_label<-as.factor(rownames(data))
+    # 
+    # #remove duplicates:
+    # data_unique <- unique(data)
+    # 
+    # #  Run tSNE:
+    # tSNEdata <- as.matrix(scale(data_unique))
+    # tsne <- Rtsne(tSNEdata, dims = 2,
+    #               perplexity= sliderInput$Perplexity, verbose=TRUE,
+    #               max_iter = sliderInput$Iteration)
+    # })
     output$distPlot <- renderPlot({
         # generate bins based on input$bins from ui.R
         x    <- faithful[, 2]
@@ -79,4 +109,3 @@ server <- function(input, output) {
 
 # Run the application 
 shinyApp(ui = ui, server = server)
-
