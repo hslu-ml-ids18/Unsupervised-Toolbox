b71fa67b2c1cc18c0f972d546462c6d9cae48996
Alex <akusche@icloud.com>
2019-10-13 13:16:56 +0200

    dynmaic column selection based on dataset; add heatmap, tree and data visulization tab

--- clustering_kmeans_hierachical/clustering_kmeans_hierachical.R
+++ clustering_kmeans_hierachical/clustering_kmeans_hierachical.R
@@ -27,15 +27,15 @@ x1 <- 80+rnorm(20, mean=20, sd=10)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Clustering a 2 dimensional .csv File with 3 columns(index, x, y)"),
-    br(),
+  # Application title
+  h1("Machine Learning 2 - Unsupervised Toolbox"),
+  "by",
+  strong("Giuliano Ardesi, Lisa Becker, Anastasiia Chebatarova, Axel Kandel, Alexander Kushe"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-        fileInput("file1", "Choose local CSV",
+        fileInput("file_local", "Choose local CSV",
         accept = c(
           "text/csv",
           "text/comma-separated-values,text/plain",
@@ -47,19 +47,35 @@ ui <- fluidPage(
                c("Default" = "default",
                  "Local" = "local",
                  "Online" = "online")),
-      tags$hr(),
+       
+       tags$hr(),
+       
+       uiOutput("selected_input_x_col"),
+       uiOutput("selected_input_y_col"),
+       
+
       sliderInput("k",
                         "Number of clusters:",
                         min = 1,
                         max = 20,
-                        value = 3)
+                        value = 3),
+      sliderInput("tree_h",
+                  "Height of tree:",
+                  min = 1,
+                  max = 20,
+                  value = 3)
         ),
+      
+
+      
         # Show a plot of the generated distribution
         mainPanel(
           # Output: Tabset w/ plot, summary, and table ----
           tabsetPanel(type = "tabs",
+                      tabPanel("DataSet", br(), verbatimTextOutput("summary"), verbatimTextOutput("strucutre"), tableOutput("view")),
                       tabPanel("K-means", plotOutput("k_cluster"), plotOutput("k_cluster_total") ),
-                      tabPanel("Hierachical", textOutput("hierachical"))
+                      tabPanel("Heatmap", plotOutput("heatmap")),
+                      tabPanel("Tree", plotOutput("tree"))
           )
 
         )
@@ -68,31 +84,71 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-        output$k_cluster <- renderPlot({
+  
+          # Return the requested dataset ----
+          datasetInput <- reactive({
+            switch(input$dataset,
+                   local = read.csv(file=input$file_local$datapath, header=TRUE, sep=","),
+                   online = read.csv(url(input$file_online), header=TRUE, sep=","),
+                   data_def)
+          })
           
-          # Choose Dataset (local, online, default, generate)
-          inFile <- input$file1
-          data <- switch(input$dataset,
-                 default = data_def,
-                 local = read.csv(file=inFile$datapath, header=TRUE, sep=","),
-                 online = read.csv(url(input$file_online), header=TRUE, sep=","),
-                 data_def)  
+          selectedData <- reactive({
+            datasetInput()[, c(input$xcol, input$ycol)]
+          })
+          
+          #output$selected_input_xclol <- renderText({names(datasetInput())})
+          ### This will create the dynamic dropdown list ###
+          
+
+          
+          output$selected_input_x_col <- renderUI({
+            selectInput('xcol', 'X Variable', names(datasetInput()))
+          })
+          output$selected_input_y_col <- renderUI({
+            selectInput('ycol', 'Y Variable', names(datasetInput()), selected=names(datasetInput())[[2]])
+          })
+          
+          output$heatmap <- renderPlot({
+            data_scaled <- as.matrix(scale(datasetInput()))
+            heatmap(data_scaled,
+                    col = topo.colors(200, alpha=0.5),
+                    Colv=F, scale="none")
+          })
+          
+          output$tree <- renderPlot({
+            data_scaled <- as.matrix(scale(datasetInput()))
+            hc1= hclust(dist(data_scaled))
+            cutree(hc1, k = 3)
+            plot(hc1)
+            
+          })
+  
+         output$k_cluster <- renderPlot({
+          
+          # read local, online or defautl dataset
+          data <- datasetInput()
           
           # Data prep, only numeric
           data <- data[ , purrr::map_lgl(data, is.numeric)]
           
           # Choose column 2 and 3, skipping column 1, assuming its an index.
-          data <- data[2:3]
-          
+          data <- selectedData()
+    
           # Run K-means Plot
           km.out <- kmeans(data,input$k,nstart =50)
           plot (data, col =(km.out$cluster+1),
           main ="K-Means Clustering" ,
           xlab =names(data)[1] , ylab =names(data)[2] ,
           pch =20 , cex =2)
+          points(km.out$centers, pch = 4, cex = 4, lwd = 4)
       
         })
+        
         output$k_cluster_total <- renderPlot({    
+          #read dataset
+          data <- datasetInput()
+          
           # Run Total within-cluster sum of squares Plot
           List_y <- list()
           List_x <- list()
@@ -106,22 +162,30 @@ server <- function(input, output) {
           xlab ="Number of Clusters", 
           ylab ="Total within-cluster sum of squares")
         })
-        output$hierachical <- renderText({  
         
-        # ************
-        # HIERACHICAL 
-        # ************
-          scaled_data = as.matrix(scale(data))
-          
-          hc1= hclust(dist(scaled_data))
-          plot(hc1)
-          heatmap(scaled_data, Colv = F, scale = "none")
-          
-          thc1 = hclust(dist(t(scaled_data)))
-          plot(thc1)
-          
-        # *************
+        # Generate a summary of the dataset
+        output$summary <- renderPrint({
+          data <- datasetInput()
+          summary(data)
         })
+        
+        # Show the first "n" observations
+        #output$view <- renderTable({
+        #  input$show_all = "head"
+        #})
+        #  head(datasetInput())
+        
+        # Return the requested dataset ----
+        output$view <- renderTable({
+          head(datasetInput())
+        })
+
+        
+        # Show the first "n" observations
+        output$strucutre <- renderPrint({
+          str(datasetInput())
+        })
+      
 }
 
 # Run the application 
